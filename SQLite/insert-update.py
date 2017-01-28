f2913167ea54ec76379ee08779c141af69450527
Jacques Berger <jacques.berger@code3.ca>
2017-01-28 01:52:00 -0500

    Mettre la lecture dans la transaction

--- SQLite/insert-update.py
+++ SQLite/insert-update.py
@@ -19,10 +19,10 @@ import sqlite3
 connection = sqlite3.connect('musique.db')
 cursor = connection.cursor()
 cursor.execute("insert into artiste(nom, est_solo, nombre_individus) values('Brain Surgery', 0, 7)")
-connection.commit()
 
 cursor.execute("select last_insert_rowid()")
 lastId = cursor.fetchone()[0]
+connection.commit()
 
 cursor.execute("update artiste set nom = 'Unstoppable' where id = %d" % lastId)
 connection.commit()
